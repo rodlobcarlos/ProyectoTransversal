64de68caeb90e4d4a508695a4ff91c13da00cba7
Carlos Rodríguez Lobato <rodlobcarlos@gmail.com>
2026-02-16 12:44:10 +0100

    new project

---
+++ GestionVuelos/src/main/java/com/project/GestionVuelos/GestionVuelosApplication.java
@@ -0,0 +1,13 @@
+package com.project.GestionVuelos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionVuelosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionVuelosApplication.class, args);
+	}
+
+}
