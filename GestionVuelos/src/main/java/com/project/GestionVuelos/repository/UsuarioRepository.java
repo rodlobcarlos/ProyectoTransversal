2d6bbb1f889532b46de66dd7c9ba1e214895114d
rodlobcarlos <rodlobcarlos@gmail.com>
2026-03-18 20:31:08 +0100

    Update UsuarioRepository.java

--- GestionVuelos/src/main/java/com/project/GestionVuelos/repository/UsuarioRepository.java
+++ GestionVuelos/src/main/java/com/project/GestionVuelos/repository/UsuarioRepository.java
@@ -7,7 +7,7 @@ import com.project.GestionVuelos.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-	Usuario findByNombre(String username);
+	Usuario findByUsuario(String username);
 
-	boolean existsByNombre(String username);
+	boolean existsByUsuario(String username);
 }
